08bc32ff10cd67e91864818d71136d31965e12d1
unknown <boris.galac@gmail.com>
2023-12-14 23:30:16 +0100

    asd

--- wp-content/themes/aljmas/page-galerija.php
+++ wp-content/themes/aljmas/page-galerija.php
@@ -7,9 +7,9 @@
                 <img src="/wp-content/themes/aljmas/src/assets/images/gallery-img.png" alt="gallery images"
                     class="gallery__img mb-48" />
             </div>
-            <div class="galerija__body-wrapper mb-64">
+            <div class="galerija__body-wrapper">
                 <div class="galerija__tab">
-                    <div class="galerija__tab-btns-wrapper mb-64">
+                    <div class="galerija__tab-btns-wrapper mb-90">
                         <button class="galerija__tab-btn" data-active="false" id="album-1">
                             <img class="gallery-icon" src="/wp-content/themes/aljmas/src/assets/icons/gallery-icon.svg"
                                 aria-hidden="true">
@@ -20,21 +20,21 @@
                         <button class="galerija__tab-btn" data-active="false" id="album-2">
                             <img class="gallery-icon" src="/wp-content/themes/aljmas/src/assets/icons/gallery-icon.svg"
                                 aria-hidden="true">
-                            <span class="galerija__tab-span">Album:</span>'Otvorenje Memorijalnog centra Aljmaš' <img
+                            <span class="galerija__tab-span">Album:</span>'Aljmaš i hodočasnici' <img
                                 class="close-tab-icon"
                                 src="/wp-content/themes/aljmas/src/assets/icons/close-tab-icon.svg" aria-hidden="true">
                         </button>
                         <button class="galerija__tab-btn" data-active="false" id="album-3">
                             <img class="gallery-icon" src="/wp-content/themes/aljmas/src/assets/icons/gallery-icon.svg"
                                 aria-hidden="true">
-                            <span class="galerija__tab-span">Album:</span>'Otvorenje Memorijalnog centra Aljmaš' <img
+                            <span class="galerija__tab-span">Album:</span>'Nuncij Giorgio Lingua u Aljmašu 2023.' <img
                                 class="close-tab-icon"
                                 src="/wp-content/themes/aljmas/src/assets/icons/close-tab-icon.svg" aria-hidden="true">
                         </button>
                         <button class="galerija__tab-btn" data-active="false" id="album-4">
                             <img class="gallery-icon" src="/wp-content/themes/aljmas/src/assets/icons/gallery-icon.svg"
                                 aria-hidden="true">
-                            <span class="galerija__tab-span">Album:</span>'Otvorenje Memorijalnog centra Aljmaš' <img
+                            <span class="galerija__tab-span">Album:</span>'Velika Gospa u Aljmašu' <img
                                 class="close-tab-icon"
                                 src="/wp-content/themes/aljmas/src/assets/icons/close-tab-icon.svg" aria-hidden="true">
                         </button>
